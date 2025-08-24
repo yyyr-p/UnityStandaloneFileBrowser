0ba23d4b6d67bbd8bb7e29cedda54aa01ac7ffc1
yyyr <61129763+yyyr-p@users.noreply.github.com>
2025-08-24 08:12:33 +0800

    feat: StandaloneFileBrowserWebGL

---
+++ Assets/StandaloneFileBrowser/StandaloneFileBrowserWebGL.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_WEBGL && !UNITY_EDITOR
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine.EventSystems;
+
+namespace SFB
+{
+    public static class StandaloneFileBrowserWebGL
+    {
+        [DllImport("__Internal")]
+        public static extern void UploadFile(string gameObjectName, string methodName, string filter, bool multiple);
+        [DllImport("__Internal")]
+        public static extern void DownloadFile(string gameObjectName, string methodName, string filename, byte[] byteArray, int byteArraySize);
+    }
+}
+#endif
